2bb071a26ae71ebe29f85cb9393bf925253b4e92
Svnth3 <marinsfraga@gmail.com>
2025-07-01 21:39:31 -0300

    Atualizando

--- aula11/roubo_carga.py
+++ aula11/roubo_carga.py
@@ -19,51 +19,107 @@ try:
     df_ocorrencia_roubo = df_roubo.groupby('munic')['roubo_carga'].sum().reset_index()
 
    
-    print(df_ocorrencia_roubo)
-
 
 except Exception as e:
     print(f'Erro: {e}')
 
 try:
     print("Obtendo informações do roubo de carga.")
-    
-    array_roubo_carga = np.array(df_roubo['roubo_carga'])
 
-    # print(filtro_data)
+    array_roubo_carga = np.array(df_ocorrencia_roubo['roubo_carga'])
+
 
     media_roubo_carga = np.mean(array_roubo_carga)
     mediana_roubo_carga = np.median(array_roubo_carga)
-    distancia = abs((mediana_roubo_carga) / mediana_roubo_carga)
+    distancia = abs((media_roubo_carga) / mediana_roubo_carga)
+    print(f'distancia: {distancia}')
 
-    # print(f'media {media_roubo_carga:.2f}')
-    # print(f'mediana: {mediana_roubo_carga:.2f}')
-    # print(f'Distância da media e mediana: {distancia}')
 
-    q1 = np.quantile(array_roubo_carga, 0.25, method='weibull')
-    q2 = np.quantile(array_roubo_carga, 0.50, method='weibull')
-    q3 = np.quantile(array_roubo_carga, 0.75, method='weibull')
 
-    # print('Q1:', q1)
-    # print('Q2:', q2)
-    # print('Q3:', q3)
+    q1 = np.quantile(array_roubo_carga, 0.25)
+    q2 = np.quantile(array_roubo_carga, 0.50)
+    q3 = np.quantile(array_roubo_carga, 0.75)
+
+
+    iqr = q3 - q1
+    limite_inferior = q1 - (iqr * 1.5)
+    limite_superior = q3 + (iqr * 1.5)
+
+    print(f'iqr: {iqr}')
+    print(f'limite inferior: {limite_inferior}')
+    print(f'limite superior: {limite_superior}')
 
     maximo = np.max(array_roubo_carga)
     minimo = np.min(array_roubo_carga)
     amplitude_total = maximo - minimo
 
-    # print('Maximo:', maximo)
-    # print('Minimo:', minimo)
-    # print('Amplitude:', amplitude_total)
+    assimetria = df_ocorrencia_roubo['roubo_carga'].skew()
+    curtose = df_ocorrencia_roubo['roubo_carga'].kurtosis()
 
-    # df_base_roubo_carga_menores = df_base_roubo_carga[[df_base_roubo_carga] < q1]
-    # df_base_roubo_carga_maiores = df_base_roubo_carga[[df_base_roubo_carga] < q3]
+    df_base_roubo_carga_menores = df_ocorrencia_roubo[df_ocorrencia_roubo['roubo_carga'] < q2]
+    df_base_roubo_carga_maiores = df_ocorrencia_roubo[df_ocorrencia_roubo['roubo_carga'] > q3]
 
     variancia = np.var(array_roubo_carga)
     distancia_variancia_media = variancia / (media_roubo_carga **2)
     desvio_padrao = np.std(array_roubo_carga)
-    coeficiente_variação = desvio_padrao / media_roubo_carga
-    
-except Exception as a:
-    print(f'Erro: {a}')
+    coeficiente_variacao = desvio_padrao / media_roubo_carga
+
+    df_roubo_carga_outliers_inferiores = df_ocorrencia_roubo[df_ocorrencia_roubo['roubo_carga']<limite_inferior]
+    df_roubo_carga_outliers_superiores = df_ocorrencia_roubo[df_ocorrencia_roubo['roubo_carga']<limite_superior]
+
+    # print(f'media {media_roubo_carga:.2f}')
+    # print(f'mediana: {mediana_roubo_carga:.2f}')
+    # print(f'Distância da media e mediana: {distancia}')
+
+    # print('Q1:', q1)
+    # print('Q2:', q2)
+    # print('Q3:', q3)
+
+    # print('Maximo:', maximo)
+    # print('Minimo:', minimo)
+    # print('Amplitude:', amplitude_total)
+
+    print('Maiores:', df_base_roubo_carga_maiores)
+    print('Menores:', df_base_roubo_carga_menores)
+
+    print("\nQUARTIS")
+    print(f'Q1: {q1}')
+    print(f'Q2: {q2}')
+    print(f'Q3: {q3}')
+    print(f'IQR: {iqr}')
+    print(f'Limite inferior: {limite_inferior}')
+    print(f'Limite superior: {limite_superior}')
+
+    print(f'Máximo: {maximo}')
+    print(f'Mínimo: {minimo}')
+    print(f'Amplitude total: {amplitude_total}')
+    print(f'Variancia: {variancia}')
+    print(f'Distância entre variância e média: {distancia_variancia_media}')
+    print(f'Desvio padrão: {desvio_padrao}')
+    print(f'Coeficiente de variação: {coeficiente_variacao}')
+    print(f'Assimetria : {assimetria}')
+    print(f'Curtose: {curtose}')
+
+    print("\nMENORES ROUBOS")
+    print(df_base_roubo_carga_menores.sort_values(by='roubo_carga', ascending=True))
+    print("\nMAIORES ROUBOS")
+    print(df_base_roubo_carga_maiores.sort_values(by='roubo_carga', ascending=False))
+
+    print("\nOUTLIERS INFERIORES")
+    if len(df_roubo_carga_outliers_inferiores) > 0:
+    print(df_roubo_carga_outliers_inferiores.sort_values(by='roubo_carga', ascending=False))
+    else:
+    print("Não há outliers inferiores")
+
+    # OUTLIERS SUPERIORES
+    print("\nOUTLIERS SUPERIORES")
+    if len(df_roubo_carga_outliers_superiores) > 0:
+    print(df_roubo_carga_outliers_superiores.sort_values(by='roubo_carga', ascending=False))
+    else:
+    print("Não há outliers superiores")
+except Exception as e:
+    print(f'Erro ao obter informações sobre padrão de roubo de cargas: {e}')
+    exit()
+
+
 
---
+++ aula11/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+print(f'q1: {q1}')
+print(f'q2: {q2}')
+print(f'q3: {q3}')
